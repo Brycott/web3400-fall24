7b603f054e3563f798660e5f43aa0dd90bc8bbda
Bryce Cottam <Bcottam2@gmail.com>
2024-09-27 16:49:38 +0000

    Update assignment 04

---
+++ assignments/04/templates/nav.php
@@ -0,0 +1,52 @@
+<body class="has-navbar-fixed top">
+    <!-- BEGIN PAGE HEADER -->
+    <header class="container">
+
+        <!-- BEGIN MAIN NAV -->
+        <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="#">
+                    <span class="icon-text">
+                        <span class="icon">
+                           <i class="fas fa-yin-yang fa-lg"></i>
+                        </span>
+                        <span>&nbsp;Balance</span>
+                    </span>
+                </a>
+                <a class="navbar-burger" aria-label="menu" aria-expanded="false">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+            <div class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item" href="#">Home</a>
+                    <a class="navbar-item" href="#">About</a>
+                </div>
+                <div class="nabar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <a class="button is-primary">Contact us</a>
+                            <a class="button is-light">Log in</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <!-- END MAIN NAV -->
+        <section class="block">&nbsp;<!--only for spacing purposes--></section>
+        <!-- BEGIN HERO -->
+        <section class="hero is-primary">
+         <div class="hero-body">
+            <p class="title">Hero title</p>
+            <p class="subtitle">Hero subtitle</p>
+         </div>
+</section>
+        <!-- END HERO -->
+
+    </header>
+    <!-- END PAGE HEADER -->
+
+    <!-- BEGIN MAIN PAGE CONTENT -->
+    <main class="container">
